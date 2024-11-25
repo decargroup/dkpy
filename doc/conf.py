17caf4189873bc2ea09a44feb273b4e1df9f8658
Steven Dahdah <sdahdah@users.noreply.github.com>
2024-11-25 14:26:08 -0500

    Update pre-commit hook

--- doc/conf.py
+++ doc/conf.py
@@ -6,57 +6,56 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'dkpy'
-copyright = '2024, Steven Dahdah and James Richard Forbes'
-author = 'Steven Dahdah and James Richard Forbes'
-release = '0.1.4'
+project = "dkpy"
+copyright = "2024, Steven Dahdah and James Richard Forbes"
+author = "Steven Dahdah and James Richard Forbes"
+release = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'matplotlib.sphinxext.plot_directive',
-    'sphinx_rtd_theme',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "matplotlib.sphinxext.plot_directive",
+    "sphinx_rtd_theme",
 ]
 
 autodoc_default_options = {
-    'members': True,
-    'inherited-members': True,
-    'show-inheritance': True,
-    'member-order': 'groupwise',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "inherited-members": True,
+    "show-inheritance": True,
+    "member-order": "groupwise",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
 
-autodoc_typehints = 'description'
+autodoc_typehints = "description"
 
 autosummary_generate = True
 
 # Path to ``objects.inv``
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
-    'matplotlib': ('https://matplotlib.org/stable/', None),
-    'control': ('https://python-control.readthedocs.io/en/latest/', None),
-    'cvxpy': ('https://www.cvxpy.org/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "control": ("https://python-control.readthedocs.io/en/latest/", None),
+    "cvxpy": ("https://www.cvxpy.org/", None),
 }
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    'collapse_navigation': False,
+    "collapse_navigation": False,
 }
-html_static_path = ['_static']
+html_static_path = ["_static"]
