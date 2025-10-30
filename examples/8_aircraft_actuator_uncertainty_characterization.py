9e7b21fb3d684ba3a64abecab65db7ea8be38bdd
Timothy <timadams1432@gmail.com>
2025-10-30 18:07:56 -0400

    Modifiy aircraft actuator uncertainty characterization example

--- examples/8_aircraft_actuator_uncertainty_characterization.py
+++ examples/8_aircraft_actuator_uncertainty_characterization.py
@@ -1,5 +1,6 @@
 """Multi-model aircraft actuator uncertainty characterization from frequency response
 data.
+
 """
 
 from matplotlib import pyplot as plt
@@ -18,7 +19,7 @@ def example_aircraft_uncertainty_characterization():
     omega = eg["omega"]
 
     # Compute the residual response for different uncertainty models
-    complex_response_residual_dict = dkpy.compute_uncertainty_residual_response(
+    response_residual = dkpy.compute_uncertainty_residual_response(
         response_actuator,
         response_actuator_offnom_list,
         {"additive", "multiplicative_input", "inverse_multiplicative_input"},
@@ -27,7 +28,7 @@ def example_aircraft_uncertainty_characterization():
     # Compute the optimal uncertainty weights with a given structure
     response_weight_left, response_weight_right = (
         dkpy.compute_uncertainty_weight_response(
-            complex_response_residual_dict["multiplicative_input"],
+            response_residual["multiplicative_input"],
             "scalar",
             "identity",
         )
@@ -37,28 +38,25 @@ def example_aircraft_uncertainty_characterization():
     weight_left = dkpy.fit_uncertainty_weight(response_weight_left, omega, 1)
 
     # Plot: Singular value response of nominal and off-nominal systems
-    fig, _, _ = dkpy.plot_singular_value_response_uncertain_model_set(
+    dkpy.plot_singular_value_response_uncertain_model_set(
         response_actuator, response_actuator_offnom_list, omega, hz=True
     )
-    fig.savefig("singular_value_plot.png")
 
     # Plot: Comparison of singular value response of uncerainty residuals for each
     # uncertainty model
-    fig, _, _ = dkpy.plot_singular_value_response_residual_comparison(
-        complex_response_residual_dict, omega, hz=True
+    dkpy.plot_singular_value_response_residual_comparison(
+        response_residual, omega, hz=True
     )
-    fig.savefig("residual_comparison.png")
 
     # Plot: Magnitude response of uncertainty weight frequency response and overbounding
     # fit
-    fig, _, _ = dkpy.plot_magnitude_response_uncertainty_weight(
+    dkpy.plot_magnitude_response_uncertainty_weight(
         response_weight_left,
         response_weight_right,
         omega,
         weight_left=weight_left,
         hz=True,
     )
-    fig.savefig("weight.png")
     plt.show()
 
 
